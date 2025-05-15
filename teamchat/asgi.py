33527141106baf7bb9ea5e682ccfd260ebf0317b
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-05-15 21:15:21 +0530

    asgi changes

--- teamchat/asgi.py
+++ teamchat/asgi.py
@@ -1,24 +1,18 @@
-"""
-ASGI config for teamchat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-# teamchat/asgi.py
-# teamchat/asgi.py
-
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teamchat.settings')
 
+application = get_asgi_application()
+
+# Import routing after settings and application are ready
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
+from channels.auth import AuthMiddlewareStack
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": application,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
