e47a0f015d4efb745c5cc195c7eca394e4528c46
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-05-17 18:48:36 +0530

    Fix ASGI import order to avoid Apps aren't loaded yet

--- teamchat/asgi.py
+++ teamchat/asgi.py
@@ -1,16 +1,20 @@
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teamchat.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teamchat.settings")
+
+# Initialise Django first
+django.setup()
+
+# Now import routing _after_ setup
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter(chat.routing.websocket_urlpatterns)
     ),
 })
